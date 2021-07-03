5275644fdca9cc78dd53fc6bef29c8a736b3fe1a
doncream <lamar.mcmiller@gmail.com>
2021-07-02 23:29:14 -0400

    partial navbar

--- ecommerce-frontend/pages/_app.js
+++ ecommerce-frontend/pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <content>
+      <Navbar />
+      <Component {...pageProps} />
+    </content>
+  )
 }
 
 export default MyApp
