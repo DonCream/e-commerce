720e9ee38de41c9f2011d93d5c7d3704217c29c3
Don Creamy <webjellystudios@gmail.com>
2021-07-30 00:16:25 -0400

    nav fixes, login working

--- ecommerce-frontend/components/Navbar.jsx
+++ ecommerce-frontend/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   return(
     <div>
     
-       <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black shadow">
+       <nav className="top-0 z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black shadow">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link href="/">
